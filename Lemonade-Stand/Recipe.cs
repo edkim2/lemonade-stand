12039226f9ea26a33576b4260da5c95d1f9308bf
edkim2 <ekim10203@gmail.com>
2018-02-28 11:32:09 -0600

    Almost done with the recipe portion.

--- Lemonade-Stand/Recipe.cs
+++ Lemonade-Stand/Recipe.cs
@@ -9,6 +9,11 @@ namespace Lemonade_Stand
     class Recipe
     {
         // member variables (HAS A)
+        public int lemon;
+        public int cup;
+        public int ice;
+        public int sugar;
+        public int priceOfLemonade;
 
         // constructor (SPAWNER)
         public Recipe()
@@ -17,5 +22,34 @@ namespace Lemonade_Stand
         }
 
         // member methods (CAN DO)
+        public int ChoosingLemons()
+        {
+            Console.WriteLine("How many lemons would you like to use for your recipe?");
+            lemon = int.Parse(Console.ReadLine());
+            return lemon;
+        }
+
+        public int ChoosingCups()
+        {
+            Console.WriteLine("How many cups of lemonade would you like to make?");
+            cup = int.Parse(Console.ReadLine());
+            return cup;
+        }
+
+        public int ChoosingIce()
+        {
+            Console.WriteLine("How much ice do you want per lemonade?");
+            ice = int.Parse(Console.ReadLine());
+            return ice;
+        }
+
+        public int ChoosingSugar()
+        {
+            Console.WriteLine("How much sugar do you want per lemonade?");
+            sugar = int.Parse(Console.ReadLine());
+            return sugar;
+        }
+
+
     }
 }
