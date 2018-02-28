b2eec4d7f569759b3bd80ec3589224f7f94ce711
edkim2 <ekim10203@gmail.com>
2018-02-28 08:30:46 -0600

    Worked on the day class to display the information gathered from the weather class.

--- Lemonade-Stand/Day.cs
+++ Lemonade-Stand/Day.cs
@@ -9,13 +9,29 @@ namespace Lemonade_Stand
     class Day
     {
         // member variables (HAS A)
+        public int day;
+        public Weather weather;
 
         // constructor (SPAWNER)
         public Day()
         {
-
+            day = 1;
+            weather = new Weather();
         }
 
         // member methods (CAN DO)
+        public void GetActualWeather(Random random)
+        {
+            weather.SetWeatherTemperature(random);
+            weather.SetWeatherCondition(random);
+            weather.DisplayActualWeather();
+        }
+
+        public void GetForecastWeather(Random random)
+        {
+            weather.SetWeatherTemperature(random);
+            weather.SetWeatherCondition(random);
+            weather.DisplayForecastWeather();
+        }
     }
 }
